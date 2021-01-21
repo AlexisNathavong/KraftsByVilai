43be418d41ecb911cd4e81e9fce62d57cd3ad0fd
Alexis Nathavong <alexispanyathong@Alexiss-Air.lan>
2021-01-20 21:07:58 -0600

    Testing...
    I just installed semantic-ui-react and I am testing it out. I am trying to figure out how to create the navbar with it.

---
+++ krafts-by-vilai/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+
+
+    return (
+        <div className='home-page'>
+            <h1>Welcome!</h1>
+                <p>Welcome to Krafts By Vilai where you can customize your wooden signs. I can customize wooden signs for special occasions such as, bridal showers, engagement parties, weddings, birthdays, etc. or just for simple decors.</p>
+        </div>
+    )
+};
+
+export default HomePage;
